496e6aba623230e29d6b2d989c18dacd521121a4
GYCboat <108936755+gtkboar@users.noreply.github.com>
2022-09-30 09:59:01 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,1 +1,17 @@
+let tabuada = 2;
+
+function escreva(){
+    
+    document.write( "Deixando de ser burro" + "<br>");
+    document.write(tabuada + " x 1 = " + tabuada + "<br>");
+    document.write(tabuada + " x 2 = " + " 4 " + "<br>");
+    document.write(tabuada + " x 3 = " + " 6 " + "<br>");
+    document.write(tabuada + " x 4 = " + " 8 " + "<br>");
+    document.write(tabuada + " x 5 = " + " 10 " + "<br>");
+    document.write(tabuada + " x 6 = " + " 12 " + "<br>");
+    document.write(tabuada + " x 7 = " + " 14 " + "<br>");
+    document.write(tabuada + " x 8 = " + " 16 " + "<br>");
+    document.write(tabuada + " x 9 = " + " 18 " + "<br>");
+    document.write(tabuada + " x 10 = " + " 20 " + "<br>");
+}
 
