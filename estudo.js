a792433408549336d1bcb50689f48a9fa80eb6b5
GYCboat <108936755+gtkboar@users.noreply.github.com>
2022-10-06 22:36:22 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,17 +1,32 @@
-let tabuada = 2;
+let tabuada = 5;
 
 function escreva(){
+    document.write( "Tabuada do " + tabuada + "<br>");
+    document.write(tabuada + " x 1 = " + (tabuada*1) + "<br>");
+    document.write(tabuada + " x 2 = " + (tabuada*2) + "<br>");
+    document.write(tabuada + " x 3 = " + (tabuada*3) + "<br>");
+    document.write(tabuada + " x 4 = " + (tabuada*4) + "<br>");
+    document.write(tabuada + " x 5 = " + (tabuada*5) + "<br>");
+    document.write(tabuada + " x 6 = " + (tabuada*6) + "<br>");
+    document.write(tabuada + " x 7 = " + (tabuada*7) + "<br>");
+    document.write(tabuada + " x 8 = " + (tabuada*8) + "<br>");
+    document.write(tabuada + " x 9 = " + (tabuada*9) + "<br>");
+    document.write(tabuada + " x 10 = " + (tabuada*10) + "<br>");
     
-    document.write( "Deixando de ser burro" + "<br>");
-    document.write(tabuada + " x 1 = " + tabuada + "<br>");
-    document.write(tabuada + " x 2 = " + " 4 " + "<br>");
-    document.write(tabuada + " x 3 = " + " 6 " + "<br>");
-    document.write(tabuada + " x 4 = " + " 8 " + "<br>");
-    document.write(tabuada + " x 5 = " + " 10 " + "<br>");
-    document.write(tabuada + " x 6 = " + " 12 " + "<br>");
-    document.write(tabuada + " x 7 = " + " 14 " + "<br>");
-    document.write(tabuada + " x 8 = " + " 16 " + "<br>");
-    document.write(tabuada + " x 9 = " + " 18 " + "<br>");
-    document.write(tabuada + " x 10 = " + " 20 " + "<br>");
-}
+} 
+ let lista = ["Jesus", "Espessato","Jorge", "Duarte","Waldete","Raquel"];
+//for(inicio,alidação,incrimento)
+ function mostra(){
+  document.write(lista.length + "<br>");
+  for (let i = 0; i < lista.length; i++){
+    document.write(lista[i] + "<br>");
+  }
+  
+ }
+
+ function mostratabuada(){
+     for(let i = 1; i <= 10; i++){
+         document.write("O valor do i " + i + "<br>");
+     }
+ }
 
