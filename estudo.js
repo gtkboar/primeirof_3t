319fa99930dd684b7ebbba83a8f34b11c6f723fb
GYCboat <108936755+gtkboar@users.noreply.github.com>
2022-10-20 10:33:18 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,7 +1,7 @@
-let tabuada = 5;
+let tabuada = 8;
 
 function escreva(){
-    document.write( "Tabuada do " + tabuada + "<br>");
+    document.write("Tabuada do " + tabuada + "<br>")
     document.write(tabuada + " x 1 = " + (tabuada*1) + "<br>");
     document.write(tabuada + " x 2 = " + (tabuada*2) + "<br>");
     document.write(tabuada + " x 3 = " + (tabuada*3) + "<br>");
@@ -12,21 +12,32 @@ function escreva(){
     document.write(tabuada + " x 8 = " + (tabuada*8) + "<br>");
     document.write(tabuada + " x 9 = " + (tabuada*9) + "<br>");
     document.write(tabuada + " x 10 = " + (tabuada*10) + "<br>");
-    
-} 
- let lista = ["Jesus", "Espessato","Jorge", "Duarte","Waldete","Raquel"];
-//for(inicio,alidação,incrimento)
- function mostra(){
-  document.write(lista.length + "<br>");
-  for (let i = 0; i < lista.length; i++){
-    document.write(lista[i] + "<br>");
-  }
-  
- }
+}
 
- function mostratabuada(){
-     for(let i = 1; i <= 10; i++){
-         document.write("O valor do i " + i + "<br>");
-     }
- }
+let lista = ["Jorge","Duarte","Waldete","Raquel",];
+
+function mostra(){
+    document.write(lista.length + "<br>");
+    for(let i = 0; i < lista.length; i++){
+        document.write(lista[i] + "<br>");
+    }
+}
+
+function mostraTabuada(){
+    for(let i = 1; i <= 10; i++){
+        document.write("O valor do i " + i + "<br>");
+    }
+}
+function total(){
+    let v = document.getElementById("valor").value;
+    let j = document.getElementById("juros").value;
+    let t = document.getElementById("meses").value;
+    let r = 0;
+    for(let i =1; i <=t; i++){
+        r = v * (1 + (j/100));
+        document.write("Mês " + i + " - valor: " + r + "<br>");
+        v = r;
+    }
+    document.write("Resultado: " + r);
+}
 
